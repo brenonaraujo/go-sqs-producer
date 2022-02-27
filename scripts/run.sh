3fda7af16cfb8a3f98b4e6ecc0fbddadfab42fc1
brenonaraujo <brenonaraujo@gmail.com>
2022-02-26 22:31:09 -0300

    fix run script and change dockerfile

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,2 @@
 
-docker run -i -d -t -p 8080:9292 -v $HOME/.aws/credentials:/home/app/.aws/credentials:ro -e AWS_DEFAULT_REGION=us-east-1 producer-sqs:snapshot
+docker run -i -d -t -p 8080:9292 -v $HOME/.aws/credentials:/appuser/.aws/credentials:ro producer-sqs:snapshot
